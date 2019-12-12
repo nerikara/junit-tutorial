83e07fadb80e3f74f8dad015476ebec245bcbcb5
nerikara <nerikara4chimi@gmail.com>
2019-12-12 20:38:21 +0900

    initial commit

---
+++ test/junit/tutorial/CalculatorTest.java
@@ -0,0 +1,14 @@
+package junit.tutorial;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest extends Calculator {
+
+    @Test
+    void multiplyで乗算結果が取得できる() {
+        fail("まだ実装されていません");
+    }
+
+}
